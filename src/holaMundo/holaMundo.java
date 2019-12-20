50f4ad46722d103efefbbfe9d35c8289b3804185
Javier Martin <jamaga@outlook.es>
2019-12-20 09:09:12 +0100

    Enlazamos con #2

---
+++ src/holaMundo/holaMundo.java
@@ -0,0 +1,13 @@
+package holaMundo;
+
+public class holaMundo {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("¡¡¡¡¡¡¡¡¡Hola mundo!!!!!!!!");
+		System.out.println("¡¡¡¡¡¡¡¡¡Hello worl!!!!!!!!");
+		System.out.println("¡¡¡¡¡¡¡¡¡Que tal!!!!!!!!");
+
+	}
+
+}
